ea4a8f235475a55e8abf0d84ccbe896d90eda481
syuu0430123456 <syuu04301@gmail.com>
2021-08-03 13:13:43 +0900

    Parag3

--- Parag3/ch3ex8.py
+++ Parag3/ch3ex8.py
@@ -1,21 +1,20 @@
-def gouhi(namae, kokugo, sansu, rika, syakai):
-    print(namae, ":", end="")
-    if kokugo>60:
-        print("国語合格")
-    else:
-        print("国語不合格")
-    if sansu>60:
-        print("算数合格")
-    else:
-        print("算数不合格")
-    if rika>60:
-        print("理科合格")
-    else:
-        print("理科不合格")
-    if syakai>60:
-        print("社会合格")
-    else:
-        print("社会不合格")
-    return
+#pandasの使い方
+#時系列データのデータフレーム、ダウンサンプリングとグラフ表示
+import pandas as pd
+import matplotlib.pyplot as plt
 
-#クラスの定義
+#csvファイルの読み込み
+mydf = pd.read_csv('TokyoTemp20082017.csv', index_col=0, parse_dates=[0], dtype='float')
+
+print(mydf.describe())
+print(mydf.head())
+print(mydf.index)
+
+mydf_down = mydf.resample('Y').meand()
+mydf_down = mydf_down.rename(columns={'Temp(C)': 'Temp_YearMean(C)'})
+print(mydf_down)
+
+#グラフ表示
+fig = mydf.plot(title='Tokyo Temprature Dataset', figsize=(8,5))
+mydf_down.plot(ax=fig)
+plt.show()
